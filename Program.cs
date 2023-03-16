3f841f7e09be00ad8ea245a954a9ce22b4d83093
Davit <dav0709@icloud.com>
2023-03-16 16:29:06 +0400

    uxagrum

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.Write("Введите число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        int temp = number;
+        int reversedNumber = 0;
+        while (temp != 0) {
+            reversedNumber = reversedNumber * 10 + temp % 10;
+            temp /= 10;
+        }
+
+        if (number == reversedNumber) {
+            Console.WriteLine("Число {0} является палиндромом", number);
+        } else {
+            Console.WriteLine("Число {0} не является палиндромом", number);
+        }
+    }
+}
